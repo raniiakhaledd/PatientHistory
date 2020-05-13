aedcafa2ba71d592552c30b1d7f7526cf3f071ad
raniiakhaledd <iraniakhaled@iCLOUd.com>
2020-05-13 15:54:57 +0200

    update

--- js/main.js
+++ js/main.js
@@ -15,80 +15,251 @@ $(document).ready(function(){
     });
     });
 
-// ------------step-wizard-------------
-$(document).ready(function () {
-    $('.nav-tabs > li a[title]').tooltip();
-    
-    //Wizard
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-
-        var target = $(e.target);
-    
-        if (target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
-
-    $(".next-step").click(function (e) {
-
-        var active = $('.wizard .nav-tabs li.active');
-        active.next().removeClass('disabled');
-        nextTab(active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var active = $('.wizard .nav-tabs li.active');
-        prevTab(active);
-
-    });
-});
-
-function nextTab(elem) {
-    $(elem).next().find('a[data-toggle="tab"]').click();
-}
-function prevTab(elem) {
-    $(elem).prev().find('a[data-toggle="tab"]').click();
-}
-
-
 
 $('.nav-tabs').on('click', 'li', function() {
     $('.nav-tabs li.active').removeClass('active');
     $(this).addClass('active');
 });
 
+
 $(document).ready(function() {
-    $("input[name$='cars']").click(function() {
+    $("input[name$='colonscopy']").click(function() {
         var test = $(this).val();
 
         $("div.desc").hide();
-        $("#Cars" + test).show();
+        $("#types" + test).show();
     });
 });
 $(document).ready(function() {
-    $("input[name$='car']").click(function() {
+    $("input[name$='mammogram']").click(function() {
         var test = $(this).val();
 
         $("div.des").hide();
-        $("#Car" + test).show();
+        $("#type" + test).show();
+    });
+});
+$(document).ready(function() {
+    $("input[name$='prc']").click(function() {
+        var test = $(this).val();
+
+        $("div.dese").hide();
+        $("#prct" + test).show();
     });
 });
 
+
 /* img */ 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
-            $('#imagePreview').hide();
-            $('#imagePreview').fadeIn(650);
+$(document).ready(function() {
+	
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
         }
-        reader.readAsDataURL(input.files[0]);
     }
-}
-$("#imageUpload").change(function() {
-    readURL(this);
+   
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
 });
 
+/*family History*/
+/*
+// Created by @conmarap.
 
+Array.prototype.search = function(elem) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] == elem) return i;
+    }
+    
+    return -1;
+};
+
+var Multiselect = function(selector) {
+    if(!$(selector)) {
+        console.error("ERROR: Element %s does not exist.", selector);
+        return;
+    }
+
+    this.selector = selector;
+    this.selections = [];
+
+    (function(that) {
+        that.events();
+    })(this);
+};
+
+Multiselect.prototype = {
+    open: function(that) {
+        var target = $(that).parent().attr("data-target");
+
+        // If we are not keeping track of this one's entries, then
+        // start doing so.
+        if(!this.selections) {
+            this.selections = [ ];
+        }
+
+        $(this.selector + ".multiselect").toggleClass("active");
+    },
+
+    close: function() {
+        $(this.selector + ".multiselect").removeClass("active");
+    },
+
+    events: function() {
+        var that = this;
+
+        $(document).on("click", that.selector + ".multiselect > .title", function(e) {
+            if(e.target.className.indexOf("close-icon") < 0) {
+                that.open();
+            }
+        });
+
+        $(document).on("click", that.selector + ".multiselect option", function(e) {
+            var selection = $(this).attr("value");
+            var target = $(this).parent().parent().attr("data-target");
+
+            var io = that.selections.search(selection);
+
+            if(io < 0) that.selections.push(selection);
+            else that.selections.splice(io, 1);
+
+            that.selectionStatus();
+            that.setSelectionsString();
+        });
+
+        $(document).on("click", that.selector + ".multiselect > .title > .close-icon", function(e) {
+            that.clearSelections();
+        });
+
+        $(document).click(function(e) {
+            if(e.target.className.indexOf("title") < 0) {
+                if(e.target.className.indexOf("text") < 0) {
+                    if(e.target.className.indexOf("-icon") < 0) {
+                        if(e.target.className.indexOf("selected") < 0 ||
+                           e.target.localName != "option") {
+                            that.close();
+                        }
+                    }
+                }
+            }
+        });
+    },
+
+    selectionStatus: function() {
+        var obj = $(this.selector + ".multiselect");
+
+        if(this.selections.length) obj.addClass("selection");
+        else obj.removeClass("selection");
+    },
+
+    clearSelections: function() {
+        this.selections = [];
+        this.selectionStatus();
+        this.setSelectionsString();
+    },
+
+    getSelections: function() {
+        return this.selections;
+    },
+
+    setSelectionsString: function() {
+        var selects = this.getSelectionsString().split(", ");
+        $(this.selector + ".multiselect > .title").attr("title", selects);
+
+        var opts = $(this.selector + ".multiselect option");
+
+        if(selects.length > 6) {
+            var _selects = this.getSelectionsString().split(", ");
+            _selects = _selects.splice(0, 6);
+            $(this.selector + ".multiselect > .title > .text")
+                .text(_selects + " [...]");
+        }
+        else {
+            $(this.selector + ".multiselect > .title > .text")
+                .text(selects);
+        }
+
+        for(var i = 0; i < opts.length; i++) {
+            $(opts[i]).removeClass("selected");
+        }
+
+        for(var j = 0; j < selects.length; j++) {
+            var select = selects[j];
+
+            for(var i = 0; i < opts.length; i++) {
+                if($(opts[i]).attr("value") == select) {
+                    $(opts[i]).addClass("selected");
+                    break;
+                }
+            }
+        }
+    },
+
+    getSelectionsString: function() {
+        if(this.selections.length > 0)
+            return this.selections.join(", ");
+        else return "Select";
+    },
+
+    setSelections: function(arr) {
+        if(!arr[0]) {
+            error("ERROR: This does not look like an array.");
+            return;
+        }
+
+        this.selections = arr;
+        this.selectionStatus();
+        this.setSelectionsString();
+    },
+};
+*/
+/* selector */
+window.onload = function(){
+    $('.ui.dropdown').dropdown();
+};
+    $('.label.ui.dropdown')
+  .dropdown();
+
+$('.no.label.ui.dropdown')
+  .dropdown({
+  useLabels: false
+});
+
+$('.ui.button').on('click', function () {
+  $('.ui.dropdown')
+    .dropdown('restore defaults')
+})
+
+/*radio*/ 
+$('.ui.radio.checkbox')
+  .checkbox()
+;
+/*checkBox*/
+$('.ui.checkbox')
+  .checkbox()
+;
+
+/* toggle Edit */
+document.querySelectorAll(".toggle__button").forEach(function(toggleButton) {
+    this.addEventListener("click", handleClickEvent, false);
+  });
+  
+  function handleClickEvent(evt) {
+    let el = evt.target;
+   
+    if (el.getAttribute("aria-checked") == "true") {
+        el.setAttribute("aria-checked", "false");
+    } else {
+        el.setAttribute("aria-checked", "true");
+    }
+}
